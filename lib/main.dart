9099303227d2acd0820da9463a87ee0ad5ef71ef
Vivek5kwt <vivek5kwt@gmail.com>
2025-09-01 11:58:47 +0530

    refactor: extract router and fix back navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
-import 'auth/login_screen.dart';
-import 'auth/signup_screen.dart';
-import 'auth/forgot_password_screen.dart';
-import 'auth/phone_number_screen.dart';
-import 'auth/otp_screen.dart';
 import 'auth/bloc/auth_bloc.dart';
 import 'api/auth_service.dart';
-import 'screens/splash_screen.dart';
-import 'screens/walkthrough_screen.dart';
-import 'screens/home_screen.dart';
-import 'screens/terms_screen.dart';
-import 'screens/privacy_screen.dart';
-import 'screens/help_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'app_router.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -28,25 +17,6 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  static final _router = GoRouter(
-    routes: [
-      GoRoute(path: '/', builder: (_, __) => const SplashScreen()),
-      GoRoute(path: '/walkthrough', builder: (_, __) => const WalkthroughScreen()),
-      GoRoute(path: '/login', builder: (_, __) => const LoginScreen()),
-      GoRoute(path: '/signup', builder: (_, __) => const SignupScreen()),
-      GoRoute(path: '/forgot', builder: (_, __) => const ForgotPasswordScreen()),
-      GoRoute(path: '/phone', builder: (_, __) => const PhoneNumberScreen()),
-      GoRoute(
-        path: '/otp',
-        builder: (_, state) => OtpScreen(confirmationResult: state.extra),
-      ),
-      GoRoute(path: '/home', builder: (_, __) => const HomeScreen()),
-      GoRoute(path: '/terms', builder: (_, __) => const TermsScreen()),
-      GoRoute(path: '/privacy', builder: (_, __) => const PrivacyScreen()),
-      GoRoute(path: '/help', builder: (_, __) => const HelpScreen()),
-    ],
-  );
-
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
@@ -58,7 +28,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        routerConfig: _router,
+        routerConfig: AppRouter.router,
       ),
     );
   }
